19e22140dc1b3123bfa4f8b2d4da2dda4f5b5d07
David Oduenye <davidoduneye1@gmail.com>
2022-11-28 17:41:47 -0500

    fixed /profile/[pid].js

--- pages/profile/[pid].js
+++ pages/profile/[pid].js
@@ -1,39 +1,22 @@
 import {useQuery} from "react-query";
-import Loading from "@/components/Loading";
-import Error from "@/components/Error";
 
 const Profile = ({ profile }) => {
     profile = profile[0];
-    const { isLoading, error, data } = useQuery("users", async () => {
-        const res = await fetch(`http://localhost:4000/api/users/${profile._id}`);
-        return res.json();
-    });
 
-    if (isLoading) return (
-        <Loading />
-    )
-
-    if (error) return (
-        <Error />
-    )
+    console.log(profile);
+    
     return (
         <div>Profile</div>
     )
-
-    return(
-      <div>
-          Hello
-      </div>
-    );
 };
 
 const getStaticProps = async ({ params: { pid } } ) => {
     const res = await fetch(`http://localhost:4000/api/users/id/${pid}`);
-    const user = await res.json();
+    const profile = await res.json();
 
-    return {
+    return {    
         props: {
-            user,
+            profile,
         },
     };
 };
@@ -43,7 +26,9 @@ const getStaticPaths = async () => {
     const users = await res.json();
 
     const paths = users.map((profile) => ({
-        params: { pid: profile._id },
+        params: {
+            pid: profile._id,
+        },
     }));
 
     return {
